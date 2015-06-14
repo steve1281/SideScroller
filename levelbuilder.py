9ac7d5d6fe54934e3e9eda6bd1a9281d267c2132
Steve Falcigno <steve1281@hotmail.com>
2015-06-14 12:18:27 -0400

    working on json formatted level file save and load.

--- levelbuilder.py
+++ levelbuilder.py
@@ -1,74 +1,153 @@
 #!/usr/bin/env python
 import math
 import pygame
+import sys
+import getopt
+import json
+
 from colors import *
 
-if  __name__ == "__main__":
-    pygame.init()
-    size = width, height = 1200, 1000
-    window = pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
-    fps = 60
-    window.fill(white)
+class LevelBuilder:
+    def __init__(self):
+        pass
 
-    pygame.display.update()
-    to_draw = []
-    draw_start_box = False
+    def load(self,argv):
+        """ load level from a file or throw an error.
+        """
+        filename =  ""
+        levelname = ""
+        try:
+            opts, args = getopt.getopt(argv,"hf:l:","file=,levelname=")
+        except getopt.GetoptError:
+            print 'levelbuilder.py -f <levelfile> -l <levelname>'
+            sys.exit(2)
 
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                running = False
-            elif (event.type == pygame.MOUSEMOTION):
-                mouse_pos = mouse_x, mouse_y = pygame.mouse.get_pos()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 3:
-                    print pygame.mouse.get_pos()
-                    event = None
-                else:
-                    rpos = mouse_pos
-                    sx = math.floor(rpos[0]/10)*10
-                    sy = math.floor(rpos[1]/10)*10
-                    pos = (sx,sy)
-                    draw_start_box = True
-            elif event.type == pygame.MOUSEBUTTONUP:
-	        if event.button == 3:
-		    event = None
-		else:
-                    rpos = mouse_pos
-                    sx = math.floor(rpos[0]/10)*10
-                    sy = math.floor(rpos[1]/10)*10
-                    final_pos = (sx,sy)
+        for opt, arg in opts:
+            if opt == '-h':
+                print 'levelbuilder.py -f <levelfile>'
+                sys.exit()
+            elif opt in ("-f", "--file"):
+                filename = arg
+            elif opt in ("-l", "--levelname"):
+                levelname = arg
+        # if the filename was not set, abort
+        if filename == "":
+            print "levelbuilder.py -f <levelfile>"
+            sys.exit(2)
 
-                    #final_pos = mouse_pos
-                    draw_start_box = False
-                    r = pygame.Rect(pos, (final_pos[0]-pos[0], final_pos[1]-pos[1]))
-                    r.normalize()
-                    to_draw += [r]
 
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    target = open("level.dat",'w')
-                    for platform in to_draw:
-                        target.write("["+str(platform).split("(")[1].split(")")[0]+", black],\n")
-                    target.close()
-                    print "level written to level.dat"
+        # read the file in, if any.
+        try:
+            with open(filename) as data_file:
+                data = json.load(data_file)
+            print "Using %s." % filename
+        except IOError:
+            print "Creating new file %s. " % filename
+            data = {}
 
-                if event.key == pygame.K_BACKSPACE:
-                    if len(to_draw):
-                        to_draw.pop()
+        # basic save level structure. It we decide to add new stuff
+        # this will add it in with a default
+        data.setdefault('meta',{}).setdefault('version', 0)
+        data.setdefault('meta',{}).setdefault('levelname', "levelname")
+        data.setdefault('blocks',[(10,20,30,40)])
+        data.setdefault('playerstart', (0,0))
+        data.setdefault('keys', [(0,0)])
+        data.setdefault('exit' ,(0,0))
+        self.data=data
+        self.filename = filename
+        self.levelname = levelname
 
+
+    def save(self):
+        """ write out the level.
+        """
+        data = self.data
+        #increase the version of the file by one.
+        data.setdefault('meta',{})['version'] += 1
+
+        # set a levelname if one is provided.
+        if self.levelname != "":
+            data.setdefault('meta',{})['levelname'] = self.levelname
+
+        with open(self.filename, 'w') as outfile:
+            json.dump(data, outfile)
+
+
+    def main(self):
+        self.load(sys.argv[1:])
+        pygame.init()
+
+        size = width, height = 1200, 1000
+        window = pygame.display.set_mode(size)
+        clock = pygame.time.Clock()
+        fps = 60
         window.fill(white)
-        if (draw_start_box):
-            pygame.draw.rect(window, red, pygame.Rect(pos, (mouse_pos[0]-pos[0], mouse_pos[1]-pos[1])))
-
-        for item in to_draw:
-            pygame.draw.rect(window, black, item)
 
         pygame.display.update()
-        clock.tick(fps)
+        to_draw = []
+        # to_draw = [line.strip() for line in open('level4.dat', 'r')]
+        # print to_draw
 
-    pygame.quit()
+        draw_start_box = False
 
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    running = False
+                elif (event.type == pygame.MOUSEMOTION):
+                    mouse_pos = mouse_x, mouse_y = pygame.mouse.get_pos()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 3:
+                        print pygame.mouse.get_pos()
+                        event = None
+                    else:
+                        rpos = mouse_pos
+                        sx = math.floor(rpos[0]/10)*10
+                        sy = math.floor(rpos[1]/10)*10
+                        pos = (sx,sy)
+                        draw_start_box = True
+                elif event.type == pygame.MOUSEBUTTONUP:
+	            if event.button == 3:
+		        event = None
+		    else:
+                        rpos = mouse_pos
+                        sx = math.floor(rpos[0]/10)*10
+                        sy = math.floor(rpos[1]/10)*10
+                        final_pos = (sx,sy)
+
+                        #final_pos = mouse_pos
+                        draw_start_box = False
+                        r = pygame.Rect(pos, (final_pos[0]-pos[0], final_pos[1]-pos[1]))
+                        r.normalize()
+                        to_draw += [r]
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        temp = []
+                        for platform in to_draw:
+                            print "["+str(platform).split("(")[1].split(")")[0]+", black],\n"
+                            temp.append((platform.top,platform.left,platform.width, platform.height,'black'))
+                        self.data['blocks'] = temp
+                        self.save()
+
+                    if event.key == pygame.K_BACKSPACE:
+                        if len(to_draw):
+                            to_draw.pop()
+
+            window.fill(white)
+            if (draw_start_box):
+                pygame.draw.rect(window, red, pygame.Rect(pos, (mouse_pos[0]-pos[0], mouse_pos[1]-pos[1])))
+
+            for item in to_draw:
+                pygame.draw.rect(window, black, item)
+
+            pygame.display.update()
+            clock.tick(fps)
+
+        pygame.quit()
+
+
+if  __name__ == "__main__":
+    (LevelBuilder()).main()
 
