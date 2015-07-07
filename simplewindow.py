4bcf3bab79ad6d99fd88bd7766ab56038a27ec05
steve1281 <steve1281@hotmail.com>
2015-07-07 00:37:41 +0000

    Pulled code for the Dialog into its own file.

--- simplewindow.py
+++ simplewindow.py
@@ -2,69 +2,12 @@
 
 import pygame
 from colors import CMap
-
-class TextSprite(pygame.sprite.Sprite):
-    def __init__(self, text):
-        pygame.sprite.Sprite.__init__(self)
-        self.open_flag = False
-        self.font = pygame.font.Font(None, 36)
-        self.setText(text)
-
-    def setText(self, text):
-        pass
-
-class Feedback(TextSprite):
-    def __init__(self, text="", x=50, y=50, width=500, height=232):
-        super(Feedback, self).__init__(text)
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-
-    def setText(self, raw_text):
-        if not self.open_flag:
-            self.image = pygame.Surface((0,0))
-            self.rect = ((0,0),(0,0))
-        else:
-            texts = list(raw_text.split("\n"))
-            box = pygame.Surface((self.width,self.height))
-            box.fill(white)
-            for i in range(len(texts)):
-                text = self.font.render(texts[i], 1, (0, 0, 0))
-                textpos = text.get_rect()
-                textpos.centery = box.get_rect().centery + 30 * i
-                textpos.centerx += 20
-                box.blit(text, textpos)
-            pygame.draw.rect(box,CMap.blue,(0, 0, self.width, self.height),2)
-            pygame.draw.rect(box, CMap.red, (5, 5, 20, 20))
-            self.image = box
-            self.rect = ((self.x, self.y),(self.width, self.height))
-
-    def evaluatePos(self, pos):
-        x = pos[0] - self.x
-        y = pos[1] - self.y
-        if (x < 0 or y < 0 or
-            x > self.width or
-            y > self.height):
-            return
-        our_pos = (pos[0]-self.x ,pos[1] -self.y)
-        # self.setText("position is = %s" % str(our_pos))
-        if our_pos[0] > 5 and our_pos[0] < 25 and our_pos[1] > 5 and our_pos[1] < 25:
-            self.image = pygame.Surface((0,0))
-            self.rect = ((0,0),(0,0))
-            self.open_flag = False
-            pygame.display.update()
-
-    def openDialog(self):
-        self.open_flag = True
-        pygame.display.update()
+from Dialog import Feedback
 
 if  __name__ == "__main__":
     pygame.init()
     size = width, height = 600, 620
     window = pygame.display.set_mode(size)
-    white = pygame.Color(255, 255, 255)
-    red = pygame.Color(255, 0, 0)
 
     rect = pygame.Rect((20,50),(100,200))
     rect2 = pygame.Rect((20,50),(200,100))
@@ -92,7 +35,7 @@ if  __name__ == "__main__":
             if event.type == pygame.KEYDOWN and event.key == pygame.K_k:
                 feedback.openDialog()
 
-        window.fill(white)
+        window.fill(CMap.white)
         window.blit(test, (20, 50),rect)
         window.blit(test, (30, 50),rect2)
         window.blit(feedback.image,feedback.rect)
